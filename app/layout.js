f4e3a821f4ae7fe825fa5f55c345df8e33130e9b
bizgrowholdings50-ctrl <bizgrowholdings50@gmail.com>
2026-04-10 19:50:07 +0500

    Fix the import issue of speed insights of vercel

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import SmoothScroll from "@/components/SmoothScroll";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
